4f0dea8c345b6ca8340ef2f6d1768741f96cb6ab
ryuhw <ryu08@3top.co.kr>
2025-02-06 17:21:54 +0900

    Refactor: 인기 상품 조회 캐싱 전략 수정 (@CacheEvict -> @CachePut)

--- src/main/java/kr/hhplus/be/server/interfaces/scheduler/ProductScheduler.java
+++ src/main/java/kr/hhplus/be/server/interfaces/scheduler/ProductScheduler.java
@@ -1,13 +1,20 @@
 package kr.hhplus.be.server.interfaces.scheduler;
 
-import org.springframework.cache.annotation.CacheEvict;
+import kr.hhplus.be.server.domain.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ProductScheduler {
 
+    private final ProductService productService;
+
     @Scheduled(cron = "0 0 0 * * *")
-    @CacheEvict("popular_products")
-    public void evictPopularProductsCache() {}
+    @CachePut("popular_products")
+    public void cachePopularProducts() {
+        productService.popularProducts();
+    }
 }
