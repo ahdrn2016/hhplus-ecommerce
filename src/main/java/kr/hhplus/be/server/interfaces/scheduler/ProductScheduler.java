4a9b3b25929855492403af7506e908797672c7f4
ryuhw <ryu08@3top.co.kr>
2025-02-10 09:29:03 +0900

    Fix: CachePut 반환값 수정

--- src/main/java/kr/hhplus/be/server/interfaces/scheduler/ProductScheduler.java
+++ src/main/java/kr/hhplus/be/server/interfaces/scheduler/ProductScheduler.java
@@ -1,10 +1,13 @@
 package kr.hhplus.be.server.interfaces.scheduler;
 
+import kr.hhplus.be.server.domain.product.ProductInfo;
 import kr.hhplus.be.server.domain.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -14,7 +17,7 @@ public class ProductScheduler {
 
     @Scheduled(cron = "0 0 0 * * *")
     @CachePut("popular_products")
-    public void cachePopularProducts() {
-        productService.popularProducts();
+    public List<ProductInfo.PopularProduct> cachePopularProducts() {
+        return productService.popularProducts();
     }
 }
